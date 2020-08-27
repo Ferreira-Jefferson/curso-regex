bcf0bd5c1d96ae08386072da071e0ca3c5ec481d
Jefferson Ferreira <jefferson.ctc.ferreira@outlook.com>
2020-08-27 20:13:31 -0300

    caracteres: add ponto file

---
+++ caracteres/ponto.js
@@ -0,0 +1,12 @@
+/* . é um coringa, válido para uma posição, ou seja, assume que onde o .(ponto) foi definido HÁ um caracter qualquer. 
+* Obs: existem excessões
+*/
+
+const texto = '1,2,3,4,5,6,a.b c!d?e'
+
+console.log(texto.match(/1.2/g))
+console.log(texto.match(/1...3/g))
+
+const notas = '8.3,7.1,8.8,10.0'
+console.log(notas.match(/8../g))
+console.log(notas.match(/.\../g))
